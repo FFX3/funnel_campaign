4b1f339cf469c0239ac45098635965f55f4e8a28
Justin <justinmcintyre42@gmail.com>
2021-04-22 20:21:54 -0300

    got grapejs intergrated in the shopify dashboard

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Heading, Page } from "@shopify/polaris";
+import GrapeEditorWrapper from '../components/grapesjs/GrapeEditor'
+
+const Index = () => (
+  <Page>
+    <Heading>I like paintball</Heading>
+    <GrapeEditorWrapper></GrapeEditorWrapper>
+  </Page>
+);
+
+export default Index;
